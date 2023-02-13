bc61dbe7f5c9b4a4ee836f9fddc1c26eb255dfb2
Mordechai <mordwein77@gmail.com>
2023-02-13 14:06:21 -0500

    commit tree b06a15a8a80468cc3825b8db5ececc4e8aba674e

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters
+    
+    def actors_list
+        actors_array = []
+        actors.each do |actor|
+            actors_array << actor.full_name
+        end
+        actors_array
+    end
 end
